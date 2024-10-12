9575bfcd5167a3600d1d6cc0e6889211939d138a
CarlosBe-hub <carlosbenenatti21@gmail.com>
2024-10-12 01:30:35 -0300

    Agregación
    Agregue un fondo gris en la parte del carrusel para que resaltara, agregue funciones en la clase JavaScript para que la imagen avance cada cierto tiempo.

--- Benenatti Automotores/carousel.js
+++ Benenatti Automotores/carousel.js
@@ -2,17 +2,38 @@ let currentIndex = 0;
 const images = document.querySelectorAll('.carousel-inner img');
 const totalImages = images.length;
 
+
+function updateCarousel() {
+    const offset = -currentIndex * 100; 
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+
 document.querySelector('.carousel-siguiente').addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % totalImages; 
     updateCarousel();
 });
+
 
 document.querySelector('.carousel-atras').addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + totalImages) % totalImages; 
     updateCarousel();
 });
 
-function updateCarousel() {
-    const offset = -currentIndex * 100; 
-    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+
+function autoAdvance() {
+    currentIndex = (currentIndex + 1) % totalImages; 
+    updateCarousel();
 }
+
+
+const autoSlideInterval = setInterval(autoAdvance, 4000);
+
+
+document.querySelector('.carousel-siguiente').addEventListener('click', () => {
+    clearInterval(autoSlideInterval); 
+});
+
+document.querySelector('.carousel-atras').addEventListener('click', () => {
+    clearInterval(autoSlideInterval); 
+});
