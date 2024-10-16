3ba47b2be244162bc11ca99ae5417ccd311c0ec8
Agustin-Benenatti <agus.benenatti09@gmail.com>
2024-10-16 19:58:50 -0300

    Avances
    Modifique el carrusel ya que  usabamos varios <img>, en lugar de eso cree un arreglo en el js para que se fuera recorriendo el mismo y mostrandose en el carrusel usando solo una etiqueta <img>

--- Benenatti Automotores/carousel.js
+++ Benenatti Automotores/carousel.js
@@ -1,4 +1,46 @@
-let currentIndex = 0;
+
+
+const imagenes = [
+    { img:"/Benenatti Automotores/img/auto3ford.webp"},
+
+    { img:"/Benenatti Automotores/img/Fiat-Cronos.jpg"},
+
+    { img:"/Benenatti Automotores/img/toyota-hilux.jpg"}
+];
+
+const carouselImage = document.getElementById('carousel-image');
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+
+let index = 0;
+
+function mostrarImg(index){
+    carouselImage.src = imagenes[index].img;
+
+}
+
+nextButton.addEventListener('click',(event) => {
+    event.preventDefault();
+    index =(index + 1) %imagenes.length;
+    mostrarImg(index);
+
+});
+
+prevButton.addEventListener('click', (event) =>{
+    event.preventDefault();
+    index = (index - 1 + imagenes.length) % imagenes.length;
+    mostrarImg(index);
+});
+
+window.addEventListener('load', () => {
+    mostrarImg(index);
+});
+
+
+
+
+
+/* let currentIndex = 0;
 const images = document.querySelectorAll('.carousel-inner img');
 const totalImages = images.length;
 
@@ -36,4 +78,4 @@ document.querySelector('.carousel-siguiente').addEventListener('click', () => {
 
 document.querySelector('.carousel-atras').addEventListener('click', () => {
     clearInterval(autoSlideInterval); 
-});
+}); */
